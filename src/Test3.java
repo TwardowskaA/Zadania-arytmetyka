ada8a6dac236fc09e6f01ec537607e188a3f0be4
Agata <twardowska123@gmail.com>
2018-07-19 19:54:41 +0200

    zadania

---
+++ src/Test3.java
@@ -0,0 +1,22 @@
+public class Test3 {
+
+    public static void main(String[] args) {
+
+        int[] tablica = {1 , 2 , 3, 3, 8, 1};
+
+        for (int i = 0; i < tablica.length; i++) {
+
+
+            if (tablica[i] != tablica[tablica.length -1 -i ]){
+                System.out.println("nie jest palindromem");
+                break;
+            }
+
+
+        }
+        System.out.println("jest palindromem");
+
+    }
+
+
+}
